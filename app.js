bfea79c99c3427710fa5704983bef14f6e15e13f
abdvkh <e-address@bk.ru>
2021-01-21 19:42:08 +0500

    For loop;

--- app.js
+++ app.js
@@ -2,29 +2,29 @@ const app = Vue.createApp({
     data(){
         return {
             showBooks: true,
-            title: "The Final Empire",
-            author: "Brandon Sanderson",
-            age: 45,
-            x: 0,
-            y: 0
-        }
+            books: [
+                {
+                    title: "The Final Empire",
+                    author: "Brandon Sanderson",
+                    age: 45
+                },
+                {
+                    title: "The Way of Kings",
+                    author: "Tim Anderson",
+                    age: 34
+                },
+                {
+                    title: "The Name of winds",
+                    author: "Jake Mordan",
+                    age: 65
+                },
+            ],
+        };
     },
 
     methods: {
         toggleShowBooks(){
             this.showBooks = !this.showBooks
-        },
-
-        handleEvent(event, data) {
-            console.log(event, event.type)
-            if (data){
-                console.log(data)
-            }
-        },
-
-        handleMouseMove(event){
-            this.x = event.offsetX
-            this.y = event.offsetY
         }
     }
 })
